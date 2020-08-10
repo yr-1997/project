e19d62feb6d36e00adc51733897240c558b92616
Yura <yuralvov754755@gmail.com>
2020-08-10 21:07:16 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
- const category = 'toys';
- console.log(`https://xuipizda.com/${category}/5`);
+ "use strict";
+
+
+//  const category = 'toys';
+//  console.log(`https://xuipizda.com/${category}/5`);
+ 
+// const answers = [];
+// answers[0] = prompt('a mamu mav?','');
+// answers[1] = prompt('a tata mav?','');
+// answers[2] = prompt('a brata mav?','');
+// console.log(answers);
+ let incr = 10,
+      decr = 10;
+      incr++;
+      decr--;
+      console.log(decr);
+      console.log(incr);
